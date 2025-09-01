2b8684a0f8a974533a862b06409ea098ea28da4b
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-09-01 14:46:09 -0300

    chore: add spring project and initial configs

---
+++ task_planner/src/test/java/miranda/gabriel/task_planner/TaskPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package miranda.gabriel.task_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
